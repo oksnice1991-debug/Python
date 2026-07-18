62b5cfa51f3438beb4f3145d605f02defd8987a6
Оксана Дубовская <oksanadubovskaa@MacBook-Air-Oksana.local>
2026-07-18 17:03:02 +0300

    Добавлены тесты для урока 6

---
+++ 06_lesson/test_lesson06_task2.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+
+def test_session_storage_auth():
+    driver = webdriver.Chrome()
+    driver.get(" https://gitflic.ru/")
+
+    user1_cookie = {
+        "name": "session",
+        "value":
+        "NDZlZTI2MjUtYWI4MC00Nzc5LWJhMzAtYmUwZTgzYWJjNDJh"
+        }
+
+    user2_cookie = {
+        "name": "session",
+        "value":
+        "NmJjYzY5ZTItZWJhYi00YzVjLTg0ZjAtYWE4MjA4NzkwODI5"
+        }
+
+    driver.add_cookie(user1_cookie)
+    driver.refresh()
+    driver.get("https://gitflic.ru/user/doksana")
+    url1 = driver.current_url
+
+    driver.add_cookie(user2_cookie)
+    driver.refresh()
+    driver.get("https://gitflic.ru/user/dubovskayabakery")
+    url2 = driver.current_url
+
+    assert url1 != url2
+    driver.quit()
